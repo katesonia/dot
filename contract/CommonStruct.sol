135bc5c426006f95082ad7dfb2e6a8e5900cc4bc
Sharu Jiang <jsrshark110@gmail.com>
2018-08-04 18:11:48 -0700

    Create CommonStruct.sol

---
+++ contract/CommonStruct.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.18;
+
+library CommonStruct {
+    
+    struct Accomplishment {
+    	address member;
+    	uint token_rewarded;
+    	string message;
+    	//uint timestamp;
+    }
+    
+}
