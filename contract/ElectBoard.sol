85ce528006ce2bda5ffe87c1cd7ecc4f41a4de43
Sharu Jiang <jsrshark110@gmail.com>
2018-08-04 23:30:09 -0700

    Update ElectBoard.sol

--- contract/ElectBoard.sol
+++ contract/ElectBoard.sol
@@ -20,28 +20,18 @@ contract ElectBoard {
 
     mapping(address => Voter) voters;
     mapping(address => Candidate) candidates;
-    Candidate[10] topCandidates;
-    uint public startDate;
-    uint public endDate;
+    Candidate[3] topCandidates;
 
     /// Create a new ballot with $(_numProposals) different proposals.
     // The election only lasts for 2 days.
     constructor(address _dotTokenAddr) public {
         dotTokenContract = DotToken(_dotTokenAddr);
-        startDate = now;
-        endDate = now + 2 days;
     }
 
     /// Give vote(the weight is the balance of the voter) to candidate $(toProposal).
     function vote(address cand) public {
         Voter storage sender = voters[msg.sender];
-        uint candidateBalance = dotTokenContract.balanceOf(cand);
-        // No right to be voted if candidate doesn't have balance.
-        if (sender.voted || candidateBalance == 0) return;
-        
         uint voterBalance = dotTokenContract.balanceOf(msg.sender);
-        //No right to vote if voter has no balance.
-        if (voterBalance == 0) return;
         
         sender.voted = true;
         sender.vote = cand;
@@ -54,7 +44,14 @@ contract ElectBoard {
         }
     }
 
-    function getBoardMembers() public constant returns (Candidate[10]) {
-        return topCandidates;
+    // Get the current top 10 candidates.
+    function getBoardMembers() public returns (address[3]) {
+        address[3] storage boardMembers;
+        for (uint8 i = 0; i < topCandidates.length; i++) {
+            if (topCandidates[i].candidateAddr != address(0)) {
+                boardMembers[i] = topCandidates[i].candidateAddr;
+            }
+        }
+        return boardMembers;
     }
 }
